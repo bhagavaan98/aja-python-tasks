e20334023e278214fcc9d8874b37d10affdb8d32
kalkuri <kalkurisaikumar@gmail.com>
2023-11-14 11:43:38 +0530

    testing

--- practice.py
+++ practice.py
@@ -1,3 +1,10 @@
 class Start:
-    def __init__(self):
-        
+    def __init__(self,age,place):
+        self.age= age
+        self.place= place
+    def get(self):
+        print('I do not understand oops')
+a=Start(18,'hyderabad')
+Start.get(19)
+print(a.age)
+print(a.place)

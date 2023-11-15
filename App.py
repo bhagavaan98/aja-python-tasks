8ed8b5cb37526b82133f1e2986da43e134536b19
Lakshmikant124 <lakshmikanthparasa1410@gmail.com>
2023-11-15 12:02:57 +0530

    perfect

--- App.py
+++ App.py
@@ -1,9 +1,18 @@
 from Database import DB
 
-v1 = DB()
 
-#v1.creating_table("student",name="varchar(30)",id="int")
-v1.insert_into_table("student",("anil",30),("Ramya",40))
+with DB("root","Lucky@143","mysql") as v1:
+    v1.creating_table("student",name="varchar(30)",id="int")
+
+
+
+
+
+
+
+
+
+# v1.insert_into_table("student",("anil",30),("Ramya",40))
 # v1.deleting_table("Lk")
 
-#v1.updating_table("student","name","raju")
+# v1.updating_table("student","name","raju")
--- Database.py
+++ Database.py
@@ -4,28 +4,44 @@ cur = con.cursor()
 
 
 class DB:
-    con = pymysql.connect(user="root",password="Lucky@143",database="mysql",port=3306)
-    cur = con.cursor()
-    def __init__(self):
-        pass
+    
+    def __init__(self,user,password,database):
+        self.user = user
+        self.password = password
+        self.database = database
+    
+    def db_connection(self):
+         
+         self.con = pymysql.connect(user=self.user,password=self.password,database=self.database)
+         self.cur = self.con.cursor()
+
+    def __enter__(self):
+         
+        self.db_connection()
+        return
+    
+    def __exit__(self,*args,**kwargs):
+         self.cur.close()
+         self.con.close()
+         
+
     def creating_table(self,table_name,**kwra):
         
         
             fields = ""
             for i,j in kwra.items():
-                fields = fields + i + " " + j + ","
+                fields = fields +i + " " + j + ","
             fields = fields[:len(fields)-1]
             querry = "create table {} ({})".format(table_name,fields)
+         
+            self.cur.execute(querry)
         
-            cur.execute(querry)
-            
-            DB.con.close()
-
+           
        
        
 
     def insert_into_table(self,table_name,*args):
-        DB.con
+        
         field = ""
         for i in args:
             field = field + str(i) + ","
@@ -34,21 +50,18 @@ class DB:
         insert_querry = "insert into {} values {}".format(table_name,field)
         print(insert_querry)
         
-        cur.execute(insert_querry)
-        DB.con.commit()
-        DB.con.close()
+        self.cur.execute(insert_querry)
+       
     
 
     def deleting_table(self,table_name):
          querry = "drop table {}".format(table_name)
-         cur.execute(querry)
-         DB.con.close()
+         self.cur.execute(querry)
+         
     
     def updating_table(self,table_name,col_name,value):
         uodated_querry = "update {} set {} =".format(table_name,col_name)
-        print(uodated_querry)
-        print(type(value))
-        cur.execute(uodated_querry+value)
-        DB.con.close()
-        DB.con.commit()
-         
+        
+        
+        self.cur.execute(uodated_querry+value)
+        

ff6468431d323e148465d7d3a40bd24e49851e21
kalkuri <kalkurisaikumar@gmail.com>
2023-11-20 09:14:13 +0530

    assingnss

--- p.py
+++ p.py
@@ -1,12 +1,7 @@
-class Cart:
-    def __init__(self):
-        self.items={}
-    def add_product(self,item_name,quantity):
-        self.items[item_name]=quantity
-    def get_items(self):
-        print(self.items)
-obj=Cart()
-obj.add_product('banana',10)
-obj.get_items()
-obj.add_product('apple',20)
-obj.get_items()
+class Stock:
+    def __init__(self,a,b):
+    
+class Purchase:
+
+
+    
--- practice.py
+++ practice.py
@@ -1,7 +1,12 @@
-class Practice:
-    def __init__(self,name,age):
-        pass
-    def get(self):
-       return name,age
-a=Practice()
-print(a.get('raju',20))
+x1 = float(input("Enter the value for x1: "))
+x2 = float(input("Enter the value for x2: "))
+x3 = float(input("Enter the value for x3: "))
+x4 = float(input("Enter the value for x4: "))
+
+# Perform the operations
+result1 = (x1 + x2) ** 2
+result2 = (x3 + x4) ** 3
+
+# Display the results
+print(f"The result of ({x1} + {x2})^2 is: {result1}")
+print(f"The result of ({x3} + {x4})^3 is: {result2}")
